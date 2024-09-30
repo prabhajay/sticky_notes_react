5156dc36338fef2b460f1896cc28485e9a87147b
Lakshmiprabha <lakshmi.prbh@gmail.com>
2024-09-29 22:13:56 -0600

    front end done

---
+++ notes/src/App.jsx
@@ -0,0 +1,8 @@
+import NotesPage from "./pages/NotesPage";
+export default function App() {
+  return (
+    <div id="app">
+      <NotesPage />
+    </div>
+  );
+}
---
+++ notes/src/components/NoteCard.jsx
@@ -0,0 +1,52 @@
+import react, { useRef, useEffect } from "react";
+import Trash from "../icons/Trash";
+
+const NoteCard = ({ note }) => {
+  //let position = JSON.parse(note.position);
+  const colors = JSON.parse(note.colors);
+  const body = JSON.parse(note.body);
+  const position = JSON.parse(note.position);
+
+  const textAreaRef = useRef(null);
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textarea) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto";
+    current.style.height = current.scrollHeight + "px";
+  };
+
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{
+          backgroundColor: colors.colorHeader,
+        }}
+      >
+        <Trash />
+      </div>
+
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
+        ></textarea>
+      </div> 
+    </div>
+  );
+};
+export default NoteCard;
